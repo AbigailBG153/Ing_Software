86a922e0356570f5107fb69a2279160f6330980d
AbigailBG153 <u20211c561@upc.edu.pe>
2024-04-17 23:02:34 -0500

    prototipo-web(sprint1)

---
+++ Config/conexion.js
@@ -0,0 +1,8 @@
+ // Selecciona el botón
+const openButton = document.getElementById('btn-inicia');
+
+ // Agrega un event listener para el clic
+openButton.addEventListener('click', function() {
+     // Redirecciona al archivo HTML en la carpeta "app"
+    window.location.href = '/LandigPage/login_register.html';
+});
