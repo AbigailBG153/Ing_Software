71307131dbaa640541d2a91e3d95e38db2eeedc5
AbigailBG153 <u20211c561@upc.edu.pe>
2024-05-24 18:40:09 -0500

    Añadida entidad Schedule, DTOs, Mapper, Enum, Controlador y Servicio
    Agregue la entidad Schedule para representar los horarios, junto con los DTOs correspondientes para manejar la transferencia de datos, un Mapper para convertir entre entidades y DTOs, un Enum para representar el estado del horario, así como el Controlador y el Servicio para gestionar las operaciones relacionadas con los horarios.

---
+++ ApiRest/v1/src/main/java/com/nutrilife/fitnessservice/service/ScheduleService.java
@@ -0,0 +1,42 @@
+package com.nutrilife.fitnessservice.service;
+
+import org.springframework.stereotype.Service;
+import com.nutrilife.fitnessservice.mapper.ScheduleMapper;
+import com.nutrilife.fitnessservice.model.dto.ScheduleResponseDTO;
+import com.nutrilife.fitnessservice.model.entity.Schedule;
+import com.nutrilife.fitnessservice.model.enums.ScheduleStatus;
+import com.nutrilife.fitnessservice.repository.ScheduleRepository;
+import jakarta.transaction.Transactional;
+import java.util.Optional;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class ScheduleService {
+
+    private final ScheduleRepository scheduleRepository;
+    private final ScheduleMapper scheduleMapper;
+
+
+    public List<ScheduleResponseDTO> getAllSchedules() {
+        List<Schedule> schedules = scheduleRepository.findAll();
+        return scheduleMapper.convertToListDTO(schedules);
+    }
+
+    @Transactional
+    public ScheduleResponseDTO updateScheduleStatus(Long scheduleId) {
+        Optional<Schedule> optionalSchedule = scheduleRepository.findById(scheduleId);
+        if (optionalSchedule.isPresent()) {
+            Schedule schedule = optionalSchedule.get();
+            schedule.setStatus(ScheduleStatus.ACTIVE);
+            Schedule updatedSchedule = scheduleRepository.save(schedule);
+            return scheduleMapper.convertToDTO(updatedSchedule);
+        } else {
+            throw new RuntimeException("Schedule not found with id " + scheduleId);
+        }
+    }
+
+    
+}
