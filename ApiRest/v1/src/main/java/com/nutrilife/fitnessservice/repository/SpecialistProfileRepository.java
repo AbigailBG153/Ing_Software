872fc9d8837faa9764abb6c8e9590ce13574417e
andreag22 <82684323+andreag22@users.noreply.github.com>
2024-05-30 19:51:51 -0500

    Filtros para Specialist Profile

--- ApiRest/v1/src/main/java/com/nutrilife/fitnessservice/repository/SpecialistProfileRepository.java
+++ ApiRest/v1/src/main/java/com/nutrilife/fitnessservice/repository/SpecialistProfileRepository.java
@@ -1,19 +1,19 @@
 package com.nutrilife.fitnessservice.repository;
-
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
 import com.nutrilife.fitnessservice.model.entity.SpecialistProfile;
-
 public interface SpecialistProfileRepository extends JpaRepository<SpecialistProfile, Long>{
 
-    @Query("SELECT sp FROM SpecialistProfile sp WHERE sp.Spe_id =:Spe_id")
-    Optional<SpecialistProfile> getSpecialistProfileById(@Param("Spe_id") Long Spe_id);
+    List<SpecialistProfile> findByName(String name);
 
-    @Query("SELECT sp FROM SpecialistProfile sp WHERE sp.Spe_name LIKE CONCAT('%', :Spe_name, '%')")
-    Optional<List<SpecialistProfile>> getSpecialistProfileByName(@Param("Spe_name") String Spe_name);
+    List<SpecialistProfile> findByOcuppation(String occupation);
+
+    List<SpecialistProfile> findByAge(Integer age);
+
+    List<SpecialistProfile> findByAgeRange(Integer minAge, Integer maxAge);
+
+    List<SpecialistProfile> findByScore(Integer score);
+
+    List<SpecialistProfile> findByScoreRange(Integer minScore, Integer maxScore);
+
 }
