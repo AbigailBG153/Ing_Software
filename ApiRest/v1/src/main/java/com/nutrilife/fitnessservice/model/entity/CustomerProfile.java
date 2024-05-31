e3363254901d63d4da64f9db53ef9f770066cf26
AbigailBG153 <u20211c561@upc.edu.pe>
2024-05-30 20:27:44 -0500

    agregar Lista de reuniones a cliente

--- ApiRest/v1/src/main/java/com/nutrilife/fitnessservice/model/entity/CustomerProfile.java
+++ ApiRest/v1/src/main/java/com/nutrilife/fitnessservice/model/entity/CustomerProfile.java
@@ -1,11 +1,15 @@
 package com.nutrilife.fitnessservice.model.entity;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -44,4 +48,7 @@ public class CustomerProfile {
     @OneToOne
     @JoinColumn(name="user_id")
     private User user;
+
+    @OneToMany(mappedBy = "customerProfile", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Meeting> meetings;
 }
