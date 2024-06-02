72970349838d16f2442c008a510870c1391d6f88
DTomasVr <diego.villafuerte.2033@gmail.com>
2024-06-01 23:25:21 -0500

    Added Swagger documentation
    - Implemented dependencies for Swagger

--- ApiRest/v1/src/main/java/com/nutrilife/fitnessservice/config/OpenApiConfig.java
+++ ApiRest/v1/src/main/java/com/nutrilife/fitnessservice/config/OpenApiConfig.java
@@ -1,5 +1,45 @@
 package com.nutrilife.fitnessservice.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.List;
+
+@Configuration
 public class OpenApiConfig {
-    
+
+    @Value("${nutrilife.openapi.dev-url}")
+    private String devUrl;
+
+    @Bean
+    public OpenAPI myOpenAPI() {
+        Server devServer = new Server();
+        devServer.setUrl(devUrl);
+        devServer.setDescription("Server URL in Development environment");
+
+        Contact contact = new Contact();
+        contact.setEmail("contact@nutrilife.com");
+        contact.setName("NutriLife");
+        contact.setUrl("https://www.nutrilife.com");
+
+        License mitLicense = new License().name("MIT License").url("https://choosealicense.com/licenses/mit/");
+
+        Info info = new Info()
+                .title("NutriLife Fitness API")
+                .version("1.0")
+                .contact(contact)
+                .description("This API exposes endpoints for managing trainings in NutriLife application.")
+                .termsOfService("https://www.nutrilife.com/terms")
+                .license(mitLicense);
+
+        return new OpenAPI()
+                .info(info)
+                .servers(List.of(devServer));
+    }
 }
