05e07e0a5b9ca7ff7b4f9158b38f424ef6f10d0c
AbigailBG153 <u20211c561@upc.edu.pe>
2024-06-10 00:37:58 -0500

    creacion de las carpetas components y modules
    * En components : esta compuesta por componentes del index
    * En modules : esta cada servicio q proporcionara nuestra aplicacion

--- Frontend_web/NutriLife-V1/src/app/app-routing.module.ts
+++ Frontend_web/NutriLife-V1/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) }, { path: 'specialist', loadChildren: () => import('./modules/specialist/specialist.module').then(m => m.SpecialistModule) }, { path: 'training', loadChildren: () => import('./modules/training/training.module').then(m => m.TrainingModule) }, { path: 'recipes', loadChildren: () => import('./modules/recipes/recipes.module').then(m => m.RecipesModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend_web/NutriLife-V1/src/app/app.module.ts
+++ Frontend_web/NutriLife-V1/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './componets/sidebar/sidebar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
