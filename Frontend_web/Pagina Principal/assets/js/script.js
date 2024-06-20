69142a1e5194226efb678f857c5a46a4d2beac6f
JhairLVS <jhairvargas611@gmail.com>
2024-06-20 18:35:14 -0500

    Fronted de trainings

--- Frontend_web/Pagina Principal/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-function toggleDropdown() {
-    var dropdownContent = document.getElementById("dropdownContent");
-    dropdownContent.classList.toggle("show");
-}
-
-document.querySelector(".notification-btn").addEventListener("click", function() {
-    
-});
